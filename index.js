0f524d817e66fbccc744acb6eaaa8033e93ad2cb
VAKURA2 <sxetiger@gmail.com>
2023-02-19 04:20:08 -0300

    proyecto curriculum

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ async function consultar(){
     let restultado = await fetch("https://randomuser.me/api/")
                             .then(Response => Response.json())
                             .then(data => data )
+                            
     console.log ( restultado )
 
 }
